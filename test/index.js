69c0e4cd7f8573a689bf431eda4a88d061f77786
Akseli Palén <akseli.palen@gmail.com>
2022-07-08 16:55:47 +0300

    impl yadog.generate

--- test/index.js
+++ test/index.js
@@ -4,13 +4,9 @@ const yadog = require('../index')
 const path = require('path')
 
 yadog.generate({
-  modules: [
-    {
-      title: 'Yadog API Docs',
-      intro: 'Welcome to Yadog documentation.',
-      name: 'yadog',
-      path: path.resolve(__dirname, '..')
-    }
-  ],
-  target: path.resolve(__dirname, '..', 'API.md')
+  entry: path.resolve(__dirname, '..'),
+  output: path.resolve(__dirname, '..', 'API.md'),
+  name: 'yadog',
+  title: 'Yadog API Docs',
+  intro: 'Welcome to Yadog documentation.'
 })
