781cbcbd631a2ff25a42c649894f6f82fff8f662
Akseli Palen <akseli.palen@gmail.com>
2022-07-21 17:43:59 +0300

    small comment improvements

--- docs/generate.js
+++ docs/generate.js
@@ -1,5 +1,5 @@
-// Test by generating Yamdog's own documentation.
-
+// Generate Yamdog's own documentation.
+//
 const yamdog = require('../index')
 const path = require('path')
 const version = require('../package.json').version
