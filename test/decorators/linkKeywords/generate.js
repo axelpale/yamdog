8c33cbe72e9c30c35ec0b13fc5c6be07c68b65c3
Akseli Palén <akseli.palen@gmail.com>
2022-09-23 16:28:41 +0300

    move test of linkKeyword decorator to a dedicated dir

---
+++ test/decorators/linkKeywords/generate.js
@@ -0,0 +1,16 @@
+const yamdog = require('../../../index')
+const path = require('path')
+
+yamdog.generate({
+  entry: path.resolve(__dirname, 'fixture.js'),
+  output: path.resolve(__dirname, '..', '..', 'output.md'),
+  earmark: 'testdog',
+  title: 'Test document title',
+  intro: 'Keywords in doc intro should not be decorated: doghouse.',
+  decorators: [
+    yamdog.decorators.linkNames(),
+    yamdog.decorators.linkKeywords({
+      'doghouse': 'https://www.example.com'
+    })
+  ]
+})
