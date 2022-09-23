14034219d5a9103545ddbd1b07285f627ea7d552
Akseli Palén <akseli.palen@gmail.com>
2022-09-23 16:38:55 +0300

    test for toc decorator

---
+++ test/decorators/toc/fixture.js
@@ -0,0 +1,27 @@
+// testdog
+//
+// Test how yamdog generates tocs with this fixture.
+//
+
+// testdog.test
+//
+// A child block of testdog.
+//
+// A list immediately before toc:
+// - should not be merged
+// - to the toc list
+
+// testdog.test.doghouse
+//
+// A block for doghouses.
+//
+
+// testdog.test.puppy
+//
+// A block for puppies.
+//
+
+// testdog.test.doggo
+//
+// A block for doggos.
+//
---
+++ test/decorators/toc/generate.js
@@ -0,0 +1,13 @@
+const yamdog = require('../../../index')
+const path = require('path')
+
+yamdog.generate({
+  entry: path.resolve(__dirname, 'fixture.js'),
+  output: path.resolve(__dirname, '..', '..', 'output.md'),
+  earmark: 'testdog',
+  title: 'Test document title',
+  intro: 'Test document intro.',
+  decorators: [
+    yamdog.decorators.toc()
+  ]
+})
