e7a9ccff9d572fd4227bfe0d1fa87d3da7c40713
Akseli Palén <akseli.palen@gmail.com>
2022-11-28 03:40:38 +0200

    test for aliases decorator

---
+++ test/decorators/aliases/fixture.js
@@ -0,0 +1,17 @@
+// testdog
+//
+// Test how yamdog decorates aliases with this test fixture.
+//
+
+// testdog.test
+// testdog.tests
+//
+// A child block of testdog with two names.
+//
+
+// @testdog.test.doghouse
+// @testdog.test.shack
+// @testdog.test.kennel
+//
+// A block with three prefixed names.
+//
---
+++ test/decorators/aliases/generate.js
@@ -0,0 +1,13 @@
+const yamdog = require('../../../index')
+const path = require('path')
+
+yamdog.generate({
+  entry: path.resolve(__dirname, 'fixture.js'),
+  output: path.resolve(__dirname, '..', '..', 'output.md'),
+  earmark: 'testdog',
+  title: 'Test document title',
+  intro: 'Testing alias blocks',
+  decorators: [
+    yamdog.decorators.aliases()
+  ]
+})
