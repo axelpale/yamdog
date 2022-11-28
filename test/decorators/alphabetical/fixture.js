9adee83d5bf56c13776606d1885838caef60f531
Akseli Palén <akseli.palen@gmail.com>
2022-11-28 03:52:32 +0200

    test for alphabetical decorator

---
+++ test/decorators/alphabetical/fixture.js
@@ -0,0 +1,29 @@
+// testdog
+//
+// Test how yamdog orders the blocks.
+//
+
+// testdog.test0
+//
+// A child block of testdog.
+//
+
+// testdog.test1
+//
+// A child block of testdog.
+//
+
+// testdog.test2.odd
+//
+// A block with blockless middle name.
+//
+
+// testdog.test0.doghouse
+//
+// A child block.
+//
+
+// testdog.test1.kennel
+//
+// A child block.
+//
