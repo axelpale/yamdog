0ad4812a5a3b16ae26468f8998cb641287dfc38f
Akseli Palén <akseli.palen@gmail.com>
2023-01-24 01:29:55 +0200

    simplify test lists generator

--- test/lists/generate.js
+++ test/lists/generate.js
@@ -6,13 +6,6 @@ const path = require('path')
 yamdog.generate({
   entry: path.resolve(__dirname, 'fixture.js'),
   output: path.resolve(__dirname, 'output.md'),
-  names: {
-    'testdog': 'testdog',
-    'test': 'testdog.test'
-  },
   title: 'Yamdog Test Docs',
-  intro: 'Test document output',
-  decorators: [
-    yamdog.decorators.linkNames()
-  ]
+  intro: 'Test document output'
 })
