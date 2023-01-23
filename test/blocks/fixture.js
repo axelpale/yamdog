840976bc89c15011c118fb054d23b95be18f1ca1
Akseli Palén <akseli.palen@gmail.com>
2023-01-23 04:39:48 +0200

    upgrade blocks test to use @ prefix

--- test/blocks/fixture.js
+++ test/blocks/fixture.js
@@ -1,15 +1,15 @@
-// testdog
+// @testdog
 //
 // Test how yamdog recognizes comment blocks with
 // this test fixture.
 //
 
-// testdog.basic
+// @testdog.basic
 //
 // A basic comment block.
 //
 
-// testdog.dense
+// @testdog.dense
 // A dense comment block syntax.
 
 // A comment block without the earmark
@@ -17,22 +17,22 @@
 //
 
 //
-// testdog.delayed
+// @testdog.delayed
 //
 // Allow empty lines at the beginning.
 
-// testdog.longtail
+// @testdog.longtail
 //
 // Allow multiple empty lines at the end.
 //
 //
 //
 
-/** testdog.jsdocblock
+/** @testdog.jsdocblock
  * Support JSDoc block comment syntax.
  */
 
 /**
- * testdog.jsdocblock
+ * @testdog.jsdocblock
  * Support JSDoc block comment syntax with an empty line at the beginning.
  */
--- test/blocks/generate.js
+++ test/blocks/generate.js
@@ -6,7 +6,6 @@ const path = require('path')
 yamdog.generate({
   entry: path.resolve(__dirname, 'fixture.js'),
   output: path.resolve(__dirname, '..', 'output.md'),
-  earmark: 'testdog',
   title: 'Test document title',
   intro: 'Test document intro.'
 })
